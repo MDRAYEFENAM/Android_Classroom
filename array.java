03fd9c9b26647a5ae384acca4927c4bf04d26a44
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:39:25 +0600

    Create array.java
    String array in java

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array
+{
+    public static void main(String[] args) {
+        String[] productArray = {"Laptop", "Mobile", "Computer", "Tablet"};
+        System.out.println( productArray.length );
+        System.out.println( productArray[0] );
+        System.out.println( productArray[1] );
+        System.out.println( productArray[2] );
+        System.out.println( productArray[3] );
+    }
+}
