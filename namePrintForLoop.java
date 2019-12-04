6cb33b07d37c7cafb3edc0eb720bd50f104dcfcc
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:48:55 +0600

    Create namePrintForLoop.java
    Name print using for loop

---
+++ namePrintForLoop.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class namePrintForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Md.Rayef Enam\n");
+        }
+    }
+}
