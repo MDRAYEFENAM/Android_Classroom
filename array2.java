0ca07155c732be556cb66432870f279ad603ad55
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:40:43 +0600

    Create array2.java
    A simple array program in java

---
+++ array2.java
@@ -0,0 +1,11 @@
+public class array2
+{
+    public static void main(String[] args) {
+        String productArray[] = new String[4];
+        System.out.println( productArray.length );
+        System.out.println( productArray[0]= "Laptop" );
+        System.out.println( productArray[1]= "Mobile" );
+        System.out.println( productArray[2]= "Computer" );
+        System.out.println( productArray[3]= "Tablet" );
+    }
+}
