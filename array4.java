6e4b1ec22dcf5da9f37f14599cde9097defe929e
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:42:39 +0600

    Create array4.java
    manually print array number

---
+++ array4.java
@@ -0,0 +1,11 @@
+public class array4
+{
+    public static void main(String[] args) {
+        int a[] = new int[4];
+        //System.out.println( productArray.length );
+        System.out.println( a[0]= 5 );
+        System.out.println( a[1]= 4 );
+        System.out.println( a[2]= 3 );
+        System.out.println( a[3]= 2 );
+    }
+}
