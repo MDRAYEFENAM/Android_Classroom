21194ba2425e3e1d7906bc35b64583de8413be13
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:43:39 +0600

    Create for_loop_array.java
    for loop and array combine program in java

---
+++ for_loop_array.java
@@ -0,0 +1,10 @@
+public class for_loop_array
+    {
+        public static void main(String[] args){
+        int a[] = {5, 4, 3, 2, 1 };
+
+        for (int i =0; i<a.length; i++){
+            System.out.print(a[i]+"\n");
+        }
+    }
+}
