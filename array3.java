12ddd8f098015870d857309d5743337c8f3505fd
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-04 13:41:41 +0600

    Create array3.java
    a simple array program to print an array number

---
+++ array3.java
@@ -0,0 +1,11 @@
+public class array3
+{
+    public static void main(String[] args) {
+        int[] a = {5, 4, 3, 2,1 };
+        //System.out.println( a.length );
+        System.out.println( a[0] );
+        System.out.println( a[1] );
+        System.out.println( a[2] );
+        System.out.println( a[3] );
+    }
+}
