acaecb3bb7e301965a9442a2bb4414afa97bcdbf
Md. Rayef Enam <rayefcse34@gmail.com>
2019-12-09 22:57:47 +0600

    Create Addition.java
    Addition/Sum program

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition{
+
+     public static void main(String[] args){
+         
+        int a = 500, b = 700, sum;
+        sum = a + b;  
+        System.out.print(sum);
+        
+     }
+}
